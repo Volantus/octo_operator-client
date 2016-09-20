98e063737e606eeef854283eac6786a8a3eb7c0c
Marius Meißner <info@marius-meissner.net>
2016-09-20 23:26:31 +0200

    Added refresh timer

--- web/js/Dashboard/DashboardController.js
+++ web/js/Dashboard/DashboardController.js
@@ -1,5 +1,7 @@
 function DashboardController()
 {
+    this.lastUpdateHeader = $('#dashBoardLastUpdateHeader');
+    this.lastUpdate = new Date();
 }
 
 DashboardController.prototype.init = function ()
@@ -7,6 +9,19 @@ DashboardController.prototype.init = function ()
     app.MapWidget.init();
 
     setInterval(function () {
-        app.MapWidget.refresh();
-    }, 2000)
+        app.DashboardController.updateLastUpdateHeader();
+    }, 200);
+
+    setInterval(function () {
+        app.MapWidget.refresh(function () {
+            app.DashboardController.lastUpdate = new Date();
+            app.DashboardController.lastUpdateHeader.html('Last update: ' + 0 + ' seconds ago');
+        });
+    }, 3000);
+};
+
+DashboardController.prototype.updateLastUpdateHeader = function ()
+{
+    var difference = (new Date().getTime() - this.lastUpdate.getTime()) / 1000;
+    this.lastUpdateHeader.html('Last update: ' + difference.toFixed(0) + ' seconds ago');
 };
