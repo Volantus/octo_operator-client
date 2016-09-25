a339f8bf5b48df8594b8ad813ed554c6badbc3a6
Marius Meißner <marius.meissner@gmail.com>
2016-09-25 16:53:47 +0200

    Added WebSocket connection to real flight controller

--- web/js/DirectControl/DirectControlController.js
+++ web/js/DirectControl/DirectControlController.js
@@ -11,23 +11,26 @@ DirectControlController.prototype.init = function ()
     app.NetworkStatusWidget.setConnectionStatus('Connecting...', '#54c8ff');
 
     var connectionStatus = $('#connectionStatus');
-    this.connection = new WebSocket('wss://html5rocks.websocket.org/echo');
 
-    this.connection.onopen = function () {
-        connectionStatus.html('Waiting for motor status ...');
-        app.NetworkStatusWidget.setConnectionStatus('Connected successfully', 'greenyellow');
-        app.DirectControlController.connection.send('Ping');
-    };
+    var networkStatus = app.NetworkStatusController.get(function (networkStatus) {
+        app.DirectControlController.connection = new WebSocket('wss://[' + networkStatus.ipAddress + ']:' + networkStatus.port);
 
-    this.connection.onerror = function (error) {
-        connectionStatus.html('Connection failed!');
-        app.NetworkStatusWidget.setConnectionStatus('Connection failed', 'red');
-        console.log('WebSocket Error ' + error);
-    };
+        app.DirectControlController.connection.onopen = function () {
+            connectionStatus.html('Waiting for motor status ...');
+            app.NetworkStatusWidget.setConnectionStatus('Connected successfully', 'greenyellow');
+            app.DirectControlController.connection.send('Ping');
+        };
 
-    this.connection.onmessage = function (e) {
-        console.log('Server: ' + e.data);
-    };
+        app.DirectControlController.connection.onerror = function (error) {
+            connectionStatus.html('Connection failed!');
+            app.NetworkStatusWidget.setConnectionStatus('Connection failed', 'red');
+            console.log('WebSocket Error ' + error);
+        };
+
+        app.DirectControlController.connection.onmessage = function (e) {
+            console.log('Server: ' + e.data);
+        };
+    });
 };
 
 DirectControlController.prototype.tearDown = function () {
