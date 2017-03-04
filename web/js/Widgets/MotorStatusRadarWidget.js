48eddf6820e57456721741f19394d4f6052bb8e5
Marius Meißner <info@marius-meissner.net>
2017-03-04 17:49:11 +0100

    Rotated radar chart to correct position

--- web/js/Widgets/MotorStatusRadarWidget.js
+++ web/js/Widgets/MotorStatusRadarWidget.js
@@ -41,7 +41,7 @@ function MotorStatusRadarWidget()
      */
     this.options = {
         animation: false,
-        startAngle:23,
+        startAngle:-23,
         legend: {
             display: false
         },
